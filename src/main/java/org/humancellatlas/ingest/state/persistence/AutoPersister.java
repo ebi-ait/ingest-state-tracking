deb97661ff3d9caedb33265899de40dea0e9458c
Rolando Fernandez <rolando@ebi.ac.uk>
2018-05-15 15:31:30 +0100

    added an auto persister

--- src/main/java/org/humancellatlas/ingest/state/persistence/AutoPersister.java
+++ src/main/java/org/humancellatlas/ingest/state/persistence/AutoPersister.java
@@ -1,7 +1,23 @@
 package org.humancellatlas.ingest.state.persistence;
 
+import lombok.AllArgsConstructor;
+import lombok.NonNull;
+import org.humancellatlas.ingest.state.monitor.SubmissionStateMonitor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
 /**
  * Created by rolando on 15/05/2018.
  */
+@Service
+@AllArgsConstructor
 public class AutoPersister {
+    private final @NonNull Persister persister;
+    private final @NonNull SubmissionStateMonitor submissionStateMonitor;
+    private static final int AUTO_PERSIST_INTERVAL_EVERY_MINUTE = 1000 * 60;
+
+    @Scheduled(fixedDelay = AUTO_PERSIST_INTERVAL_EVERY_MINUTE)
+    public void autoPersist() {
+        persister.persistStateMachines(submissionStateMonitor.getStateMachines());
+    }
 }
