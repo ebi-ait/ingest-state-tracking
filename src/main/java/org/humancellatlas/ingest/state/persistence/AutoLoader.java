b152ee45a09b50ad32cb00880592d6d6254131a6
Rolando Fernandez <rolando@ebi.ac.uk>
2018-05-22 12:05:44 +0100

    error handling and logging when auto-loading

--- src/main/java/org/humancellatlas/ingest/state/persistence/AutoLoader.java
+++ src/main/java/org/humancellatlas/ingest/state/persistence/AutoLoader.java
@@ -5,9 +5,12 @@ import lombok.NonNull;
 import org.humancellatlas.ingest.client.IngestApiClient;
 import org.humancellatlas.ingest.model.SubmissionEnvelopeReference;
 import org.humancellatlas.ingest.state.monitor.SubmissionStateMonitor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.UUID;
 
@@ -27,11 +30,19 @@ public class AutoLoader implements InitializingBean {
     private final @NonNull Persister persister;
     private final @NonNull IngestApiClient ingestApiClient;
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     public void autoLoad() {
         persister.retrieveStateMachines().forEach(stateMachine -> {
             UUID envelopeUuid = UUID.fromString(stateMachine.getId());
-            SubmissionEnvelopeReference envelopeReference = ingestApiClient.referenceForSubmissionEnvelope(envelopeUuid);
-            submissionStateMonitor.monitorSubmissionEnvelope(envelopeReference, stateMachine);
+            try {
+                SubmissionEnvelopeReference envelopeReference = ingestApiClient.referenceForSubmissionEnvelope(envelopeUuid);
+                submissionStateMonitor.monitorSubmissionEnvelope(envelopeReference, stateMachine);
+            } catch (HttpClientErrorException e) {
+                log.warn(String.format("HTTP error: Failed to retrieve envelope reference for envelope with UUID: %s", envelopeUuid.toString()), e);
+            } catch (RuntimeException e) {
+                log.error(String.format("Failed to autoload statemachine for envelope with UUID: %s", envelopeUuid.toString()), e);
+            }
         });
     }
 
