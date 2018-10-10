6d7c054caad0b73e7b09150ce84a1f25d11a65d0
Rodrey Goite <rodrey@ebi.ac.uk>
2018-10-10 15:17:37 +0100

    Enabled scheduling for state tracker.

--- src/main/java/org/humancellatlas/ingest/IngestStateTrackingApplication.java
+++ src/main/java/org/humancellatlas/ingest/IngestStateTrackingApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Javadocs go here!
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.PropertySources;
  * @date 26/11/2017
  */
 @SpringBootApplication
+@EnableScheduling
 @PropertySources({
         @PropertySource("classpath:application.properties")
 })
