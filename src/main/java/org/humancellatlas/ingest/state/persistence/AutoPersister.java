c6fa55ba89777aef375fcba89a16b6b8edf96966
Rodrey Goite <rodrey@ebi.ac.uk>
2018-10-10 15:18:30 +0100

    Added debugging message to note initialisation of persistence mechanism.

--- src/main/java/org/humancellatlas/ingest/state/persistence/AutoPersister.java
+++ src/main/java/org/humancellatlas/ingest/state/persistence/AutoPersister.java
@@ -3,6 +3,8 @@ package org.humancellatlas.ingest.state.persistence;
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import org.humancellatlas.ingest.state.monitor.SubmissionStateMonitor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -16,12 +18,17 @@ import org.springframework.stereotype.Service;
         value = "app.auto-persist.enable", havingValue = "true", matchIfMissing = true
 )
 public class AutoPersister {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AutoPersister.class);
+    private static final int AUTO_PERSIST_INTERVAL_EVERY_MINUTE = 1000 * 60;
+
     private final @NonNull Persister persister;
     private final @NonNull SubmissionStateMonitor submissionStateMonitor;
-    private static final int AUTO_PERSIST_INTERVAL_EVERY_MINUTE = 1000 * 60;
 
     @Scheduled(fixedDelay = AUTO_PERSIST_INTERVAL_EVERY_MINUTE)
     public void autoPersist() {
+        LOGGER.debug("Begin persisting state machines...");
         persister.persistStateMachines(submissionStateMonitor.getStateMachines());
     }
+
 }
