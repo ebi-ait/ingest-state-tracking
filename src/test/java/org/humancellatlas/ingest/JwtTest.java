df4a54768c51a0498588251c7aff20eec3f7c915
Amnon Khen <amnon@ebi.ac.uk>
2024-02-08 12:24:04 +0000

    dcp-967: managed access: auth access to ingest api

--- src/test/java/org/humancellatlas/ingest/JwtTest.java
+++ src/test/java/org/humancellatlas/ingest/JwtTest.java
@@ -4,26 +4,37 @@ package org.humancellatlas.ingest;
 import org.assertj.core.api.Assertions;
 import org.assertj.core.api.Condition;
 import org.humancellatlas.ingest.client.IngestApiClient;
+import org.humancellatlas.ingest.client.util.JwtCredentialsProperties;
 import org.humancellatlas.ingest.client.util.JwtService;
 import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Ignore("this is an integration test for testing locally")
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest()
+@Ignore("this is an integration test for testing locally")
 public class JwtTest {
     @Autowired
     JwtService jwtService;
-
+    @Autowired
+    JwtCredentialsProperties jwtCredentialsProperties;
     @Autowired
     IngestApiClient ingestApiClient;
-    @Test
+    @Ignore("this is an integration test for testing locally")
+//    @Test
     public void testComponentInstantiation() throws Exception {
-        Assertions.assertThat(jwtService).isNotNull();
+        assertThat(jwtService).isNotNull();
         Condition<? super String> c;
-        Assertions.assertThat(System.getenv()).containsKey("GOOGLE_APPLICATION_CREDENTIALS");
-        Assertions.assertThat(jwtService.getToken()).isNotNull();
-        ingestApiClient.verifyIngestConnection();
+        assertThat(jwtCredentialsProperties.getPrivateKey()).isNotNull();
+        assertThat(jwtCredentialsProperties.getPrivateKeyId()).isNotNull();
+        assertThat(jwtCredentialsProperties.getClientEmail()).isNotNull();
+
+//        assertThat(jwtService.getToken()).isNotNull();
+//        ingestApiClient.verifyIngestConnection();
     }
 }
+
+
+
